da24d7e9e82da2dea5e56ecee2823be404198dbb
Jorge Teixeira <jorgeteixe11@gmail.com>
2024-01-25 18:02:56 +0100

    improve modal for mobile

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -9,8 +9,11 @@ interface ModalProps {
 
 export default function Modal(props: ModalProps) {
   return (
-    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex justify-center items-center">
-      <div className="relative pb-4 bg-white rounded-lg shadow-lg w-11/12 sm:max-w-screen-sm mx-auto max-h-screen overflow-y-scroll">
+    <div
+      className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex justify-center items-center"
+      onClick={props.onClose}
+    >
+      <div className="relative pb-4 bg-white rounded-lg shadow-lg w-5/6 sm:max-w-screen-sm mx-auto max-h-screen overflow-y-scroll">
         <div className="flex justify-between pb-3 pt-4 rounded-t-lg border-b bg-slate-100">
           <div className="pl-4">{props.title}</div>
           <button className="cursor-pointer z-50 pr-4" onClick={props.onClose}>
